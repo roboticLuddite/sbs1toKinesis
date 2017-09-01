d95486cdb62bd26a6eca43b12c9fd626f352167f
paul-at-786156393318 <paulu@amazon.com>
2017-09-01 22:25:57 +0100

    added mlat support

--- src/sbs1toKinesis/exec.py
+++ src/sbs1toKinesis/exec.py
@@ -3,6 +3,7 @@ import collections
 import json
 import sbs1
 import sys
+import thread
 from boto import kinesis
 import boto.kinesis.exceptions
 from datetime import datetime
@@ -10,26 +11,20 @@ import socket
 
 
 class sbs1ToKinesisStreamer (object):
-    
-    def startStreaming(self):
-        print("Starting stream.")
-        self.kinesisConn = kinesis.connect_to_region(self.connectionDetails['kinesis']['region'])
-        
-        while True:
+	
+	def streamSbs1StreamAsync(self, port):
 		
+		while True: 
 			try: 
 				self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				self.clientsocket.connect((self.connectionDetails['sbs1']['host'], self.connectionDetails['sbs1']['port']))
+				self.clientsocket.connect((self.connectionDetails['sbs1']['host'], port))
 			except:
-				print("Unable to find SBS1 stream. Waiting 5 seconds...")
+				print("Unable to find SBS1 stream on port:"+ port +". Waiting 5 seconds...")
 				time.sleep(5)
 				continue
 			
 			kinsisPutRecordsBufferFlushIndex = datetime.now().second
-			kinesisPutRecordBuffer = []			
-			
-
-
+			kinesisPutRecordBuffer = []						
 			try: 
 				for rawSBS1String in self.linesplit():
 					#sys.stdout.write(rawSBS1String)
@@ -51,46 +46,48 @@ class sbs1ToKinesisStreamer (object):
 				print("Issue connecting to SBS1 socket. Waiting 5 seconds.")
 				time.sleep(5)		
 			print('restarting buffering')
-                        time.sleep(5)
-            
-    def linesplit(self):
-        
-        try:
-            buffer = self.clientsocket.recv(100)            
-        except ValueError:            
-            return          
-        buffering = True
-        while buffering:
-            if "\n" in buffer:
-                (line, buffer) = buffer.split("\n", 1)
-                yield line + "\n"
-            else:
-                more = self.clientsocket.recv(100)
-                if not more:
-                    buffering = False
-                else:
-                    buffer += more
-        if buffer:
-            yield buffer
-    
-    def __init__(self, connectionDetails):
-        self.connectionDetails = connectionDetails
+			time.sleep(5)
 
+	def startStreaming(self):
+		print("Starting stream.")
+		self.kinesisConn = kinesis.connect_to_region(self.connectionDetails['kinesis']['region'])
+		
+		thread.start_new_thread(self.streamSbs1StreamAsync, (self, config['sbs1']['portADSB'], ) )
+		thread.start_new_thread(self.streamSbs1StreamAsync, (self, config['sbs1']['portMLAT'], ) )
+		while 1:
+			pass
 
+	def linesplit(self):
+		try:
+			buffer = self.clientsocket.recv(100)
+		except ValueError:
+			return
+		buffering = True
+		while buffering:
+			if "\n" in buffer:
+				(line, buffer) = buffer.split("\n", 1)
+				yield line + "\n"
+			else:
+				more = self.clientsocket.recv(100)
+				if not more:
+					buffering = False
+				else:
+					buffer += more
+		if buffer:
+			yield buffer
+	
+	def __init__(self, connectionDetails):
+		self.connectionDetails = connectionDetails
 
 
 if __name__ == '__main__':
-    
-    config =  collections.defaultdict(dict)
-    
-    config['sbs1']['host'] = '192.168.2.104'
-    config['sbs1']['port'] = 30003
-    config['kinesis']['region'] = 'us-east-1'
-    config['kinesis']['streamID'] = 'sbs1Stream'
-    config['kinesis']['partionKey'] = 'underwoodpaul'
-    
-    
-    streamer = sbs1ToKinesisStreamer(config)
-    streamer.startStreaming()    
-    
-
+	
+	config =  collections.defaultdict(dict)
+	config['sbs1']['host'] = '192.168.2.104'
+	config['sbs1']['portADSB'] = 30003
+	config['sbs1']['portMLAT'] = 30106
+	config['kinesis']['region'] = 'us-east-1'
+	config['kinesis']['streamID'] = 'sbs1Stream'
+	config['kinesis']['partionKey'] = 'underwoodpaul'
+	streamer = sbs1ToKinesisStreamer(config)
+	streamer.startStreaming()
